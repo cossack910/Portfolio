72c130417b6ddf683562a9ce5dff71e6d01c4e48
toshworld <maris160zhk@gmail.com>
2019-12-21 11:02:21 +0900

    fourth commit

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,11 +1,11 @@
 class TopsController < ApplicationController
     def index
-        @gadget = Gadget.all.order(id: "DESC")
+        @gadget = Gadget.all.order(id: "DESC").page(params[:page]).per(5)
+        @new_reviews = Gadget.joins("INNER JOIN reviews ON gadgets.id = reviews.gadget_id").select("gadgets.*, reviews.id AS reviews_id")
+        @new_release_date = Gadget.all.order(release_date: "DESC").page(params[:page]).per(5)
         @phones = Gadget.where(category_id: 1).order(id: "DESC")
         @pc = Gadget.where(category_id: 2).order(id: "DESC")
         @other = Gadget.where(category_id: 3).order(id: "DESC")
-        @new_reviews = Gadget.joins("INNER JOIN reviews ON gadgets.id = reviews.gadget_id").select("gadgets.*, reviews.id AS reviews_id")
-        @new_release_date = Gadget.all.order(release_date: "ASC")
     end
 
     def lists
