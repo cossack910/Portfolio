17c8ab0b63d9fcac202de7d1f34b6b71a75acc45
toshworld <maris160zhk@gmail.com>
2019-12-25 20:36:45 +0900

    ten commit

--- app/controllers/delete_reviews_controller.rb
+++ app/controllers/delete_reviews_controller.rb
@@ -1,11 +1,12 @@
 class DeleteReviewsController < ApplicationController
-    after_action :update_review 
+    before_action :update_review 
+    
     def update
         Review.find(params[:id]).update(delete_flag: 1)
     end
 
     private
     def update_review
-        @review = Review.where(user_id: current_user.id)
+        @review = Review.find(params[:id])
     end
 end
